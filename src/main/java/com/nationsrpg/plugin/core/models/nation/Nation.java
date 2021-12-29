d9751c348c9558aed8d67ff25d1db7f79cfab751
byteful <info@byteful.me>
2021-12-28 19:09:04 -0600

    Added more data structures to Nation

--- src/main/java/com/nationsrpg/plugin/core/models/nation/Nation.java
+++ src/main/java/com/nationsrpg/plugin/core/models/nation/Nation.java
@@ -1,22 +1,23 @@
 package com.nationsrpg.plugin.core.models.nation;
 
 import com.nationsrpg.plugin.core.NationsRPGPlugin;
+import me.byteful.lib.datastore.api.data.StoredGroup;
 import me.byteful.lib.datastore.api.model.Model;
 import me.byteful.lib.datastore.api.model.impl.JSONModelId;
 import me.lucko.helper.utils.Players;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
-public record Nation(@NotNull UUID uuid, @NotNull UUID leader) implements Model {
+@StoredGroup("nations")
+public record Nation(@NotNull UUID uuid, @NotNull UUID leader, @NotNull Double balance,
+                     @NotNull Set<NationMember> members) implements Model {
   @NotNull
   public static Nation create(@NotNull UUID leader) {
     final UUID uuid = UUID.randomUUID();
-    final Nation nation = new Nation(uuid, leader);
-    NationsRPGPlugin.getInstance().getDataStore().set(JSONModelId.of("id", uuid.toString()), nation);
+    final Nation nation = new Nation(uuid, leader, 0.0D, new HashSet<>(Collections.singleton(NationMember.create(leader, NationMember.MemberRank.LEADER))));
+    nation.update();
 
     return nation;
   }
@@ -26,23 +27,34 @@ public record Nation(@NotNull UUID uuid, @NotNull UUID leader) implements Model 
     return Players.get(leader);
   }
 
+  public void update() {
+    NationsRPGPlugin.getInstance().getDataStore().set(JSONModelId.of("id", uuid.toString()), this);
+  }
+
+  public void delete() {
+    NationsRPGPlugin.getInstance().getDataStore().delete(Nation.class, JSONModelId.of("id", uuid.toString()));
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Nation nation = (Nation) o;
-    return uuid.equals(nation.uuid);
+    return uuid.equals(nation.uuid) && leader.equals(nation.leader) && balance.equals(nation.balance) && members.equals(nation.members);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(uuid);
+    return Objects.hash(uuid, leader, balance, members);
   }
 
   @Override
   public String toString() {
     return "Nation{" +
         "uuid=" + uuid +
+        ", leader=" + leader +
+        ", balance=" + balance +
+        ", members=" + members +
         '}';
   }
 }
--- src/main/java/com/nationsrpg/plugin/core/models/nation/NationStructure.java
+++ src/main/java/com/nationsrpg/plugin/core/models/nation/NationStructure.java
@@ -1,5 +1,6 @@
 package com.nationsrpg.plugin.core.models.nation;
 
+import com.google.common.reflect.TypeToken;
 import me.byteful.lib.datastore.api.model.ModelStructure;
 import me.byteful.lib.datastore.api.model.ProcessedModel;
 import me.byteful.lib.datastore.api.model.ProcessedModelFieldType;
@@ -7,22 +8,29 @@ import me.byteful.lib.datastore.api.model.impl.JSONProcessedModel;
 import me.lucko.helper.gson.GsonProvider;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class NationStructure implements ModelStructure<Nation> {
   @Override
   public @NotNull ProcessedModel serialize(@NotNull Nation nation) {
     return new JSONProcessedModel(GsonProvider.standard())
         .append("id", ProcessedModelFieldType.UNIQUE_INDEXED, nation.uuid())
-        .append("leader", ProcessedModelFieldType.UNIQUE_INDEXED, nation.leader());
+        .append("leader", ProcessedModelFieldType.UNIQUE_INDEXED, nation.leader())
+        .append("balance", ProcessedModelFieldType.NORMAL, nation.balance())
+        .append("members", ProcessedModelFieldType.NORMAL, nation.members());
   }
 
   @Override
   public @NotNull Nation deserialize(@NotNull ProcessedModel processedModel) {
-    if (processedModel instanceof JSONProcessedModel) {
-      return GsonProvider.standard()
-          .fromJson(((JSONProcessedModel) processedModel).toJSON(), Nation.class);
+    if (processedModel.has("id") && processedModel.has("leader") && processedModel.has("balance") && processedModel.has("members")) {
+      final TypeToken<HashSet<NationMember>> setToken = new TypeToken<>() {
+      };
+      return new Nation(processedModel.get("id", UUID.class).orElseThrow(), processedModel.get("leader", UUID.class).orElseThrow(), processedModel.get("balance", Double.class).orElseThrow(), (Set<NationMember>) processedModel.get("members", setToken.getType()).orElseThrow());
     }
 
-    throw new IllegalStateException("Bad data store.");
+    throw new IllegalArgumentException("Outdated model structure! Please modify database manually.");
   }
 
   @Override
